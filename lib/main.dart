7483b076b92d29fd48535558b5731e1b4d4be91f
glyphack <sh.hooshyari@cafebazaar.ir>
2019-09-03 22:40:20 +0430

    added background image to app.

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,20 @@ class ItemistHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-        title: Text('Itemistd'),
+        title: Text('Itemist'),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
         ],
       ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+
     );
   }
 }
